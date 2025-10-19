393f8032edc89e4dfa192f7270834fbfc6ba2147
Benant <benant@paran.com>
2025-10-20 02:03:20 +0900

    재생목록 DOM 변경에 대응

--- src/search_playlist.js
+++ src/search_playlist.js
@@ -2,7 +2,7 @@
 let playlistSearch = true;
 $(function () {
   // 팝업 레이어 HTML 구조 생성
-  const searchHTML = `<div id="box-search-playlist"><label>검색:</lavel><input type="text" naem="search_playlist" id="search_playlist" placeholder="검색어를 입력해주세요" autocomplete="off" class="ytSearchboxComponentInput yt-searchbox-input title" style=" margin-left: 1rem; width: 15rem;border: 1px solid gray;border-radius: 1rem;padding: 5px 1rem;"></div>`;
+  const searchHTML = `<div id="box-search-playlist" style="color:var(--yt-spec-text-primary);position: absolute;right: 6px;top: 6px;"><label>검색:</lavel><input type="text" naem="search_playlist" id="search_playlist" placeholder="검색어를 입력해주세요" autocomplete="off" class="ytSearchboxComponentInput yt-searchbox-input title" style=" margin-left: 1rem; width: 15rem;border: 1px solid gray;border-radius: 1rem;padding: 5px 1rem;"></div>`;
   // let i = 0;
   const checkAndAddSearch = () => {
     chrome.storage.sync.get(['popupView', 'playlistSearch'], (data) => {
@@ -14,19 +14,20 @@ $(function () {
     });
     if (playlistSearch) {
       // console.log(i++);
-      const targetElement = $('ytd-add-to-playlist-renderer:visible').find('#header ytd-menu-title-renderer:visible');
+      // const targetElement = $('ytd-add-to-playlist-renderer:visible').find('#header ytd-menu-title-renderer:visible');
+      const targetElement = $('yt-contextual-sheet-layout:visible').find('.ytContextualSheetLayoutHeaderContainer .ytPanelHeaderViewModelTitleHeader:visible');
       if (targetElement.length > 0) {
         const searchBox = targetElement.find('#box-search-playlist');
         if (searchBox.length < 1) {
           targetElement.append(searchHTML);
-          $('ytd-add-to-playlist-renderer').find('#search_playlist').on('keyup', function () {
+          $('yt-contextual-sheet-layout').find('#search_playlist').on('keyup', function () {
             // console.log($(this).val());
             const search_key = $.trim($(this).val());
-            $('ytd-add-to-playlist-renderer ytd-playlist-add-to-option-renderer').show();
+            $('yt-contextual-sheet-layout toggleable-list-item-view-model').show();
             if (search_key) {
               const regex = new RegExp(search_key, 'i');
-              $('ytd-add-to-playlist-renderer').find('ytd-playlist-add-to-option-renderer').each(function () {
-                const playlist_title = $.trim($(this).find('yt-formatted-string#label').text());
+              $('yt-contextual-sheet-layout').find('toggleable-list-item-view-model').each(function () {
+                const playlist_title = $.trim($(this).find('.yt-list-item-view-model__title').text());
                 if (!playlist_title.match(regex)) {
                   $(this).hide();
                 }
@@ -36,8 +37,11 @@ $(function () {
           // console.log('재생목록에 추가 에 검색 추가 완료');
         }
       } else {
-        $('ytd-add-to-playlist-renderer ytd-playlist-add-to-option-renderer').show(); // 원래대로 playlist는 모두 보이도록 
+        $('yt-contextual-sheet-layout toggleable-list-item-view-model').show(); // 원래대로 playlist는 모두 보이도록 
       }
+      // 검색 박스 노출되면 커서 이동 시키기
+      $('yt-contextual-sheet-layout').find('#search_playlist').focus();
+      $('yt-contextual-sheet-layout').find('#box-search-playlist').on('click', function(){ return false; })
     }
     setTimeout(checkAndAddSearch, 500);
   };
